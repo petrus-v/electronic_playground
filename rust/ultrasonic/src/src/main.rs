a61dd499824ce9b67330686143c1bdf86518edcb
Pierre Verkest <pverkest@anybox.fr>
2016-10-22 15:49:10 +0200

    [rust/ultrasonic]user cupi instead fs_gpio

--- rust/ultrasonic/src/src/main.rs
+++ rust/ultrasonic/src/src/main.rs
@@ -1,55 +1,47 @@
-extern crate sysfs_gpio;
+extern crate cupi;
 
 use std::thread::sleep;
 use std::time::{Duration, Instant};
-use sysfs_gpio::{Direction, Edge, Pin};
+use cupi::{CuPi, Logic};
 
 
 
 fn main() {
-    let output = Pin::new(23);
-    let input = Pin::new(24);
+    let cupi = CuPi::new().unwrap();
 
-    let mut start = Instant::now();
-    let mut elapsed_ms : f64 = -1.0;
+    let output = cupi.pin(23).unwrap().output();
+    let input = cupi.pin(24).unwrap().input();
+    output.write(Logic::Low);
 
-    input.with_exported(|| {
-        try!(input.set_direction(Direction::In));
-        try!(input.set_edge(Edge::BothEdges));
-        let mut poller = try!(input.get_poller());
-        output.with_exported(|| {
-            try!(output.set_direction(Direction::Out));
-            try!(output.set_value(0));
-            // Allow module to settle
-            sleep(Duration::from_millis(1000));
-            try!(output.set_value(1));
-            // Send 10us pulse to trigger
-            sleep(Duration::from_millis(20));
-            output.set_value(0);
-            start = Instant::now();
-            Ok(())
-        }).expect("Export gpio 23");
+    // Allow module to settle
+    sleep(Duration::from_millis(500));
 
-        loop {
-            match try!(poller.poll(1000)) {
-                Some(value) => {
-                    println!(
-                        "poll value: {}",
-                        value
-                    );
-                    if value == 0 {
-                        elapsed_ms = start.elapsed().subsec_nanos() as f64;
-                        break;
-                    } else {
-                        start = Instant::now();
-                    }
-                },
-                None => {break;}
-            };
-        }
-        Ok(())
-    }).expect("Export gpio 24");
 
+    let elapsed_ms : f64;
+
+    output.write(Logic::High);
+    // Send 10us pulse to trigger
+    sleep(Duration::from_millis(10));
+    output.write(Logic::Low);
+    let start = Instant::now();
+
+//    println!("wainting High");
+    loop {
+        match input.read() {
+            Logic::Low => {print!(".")},
+            Logic::High => {break;}
+        };
+    }
+//    println!("wainting Low");
+    loop {
+        match input.read() {
+            Logic::High=> {print!(".")},
+            Logic::Low => {
+                elapsed_ms = start.elapsed().subsec_nanos() as f64;
+                break;
+            }
+        };
+    }
     // Distance = elapsed time / 2 * velocity
     // where velocity = 340m / s
     let distance = elapsed_ms * 0.000000001 / 2.0 * 340.0;
