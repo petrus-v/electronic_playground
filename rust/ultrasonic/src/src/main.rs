fceddbca1cc2d56947db3c51626f68a257dfe98b
Pierre Verkest <pierreverkest84@gmail.com>
2016-10-22 13:10:03 +0000

    [rust/ultrasonic]make kind of working
    really needs to communicate with memory address

--- rust/ultrasonic/src/src/main.rs
+++ rust/ultrasonic/src/src/main.rs
@@ -11,24 +11,25 @@ fn main() {
     let input = Pin::new(24);
 
     let mut start = Instant::now();
-    let mut elapsed_ms : f64 = 0.0;
-
-    output.with_exported(|| {
-        try!(output.set_direction(Direction::Out));
-        try!(output.set_value(0));
-        // Allow module to settle
-        sleep(Duration::from_millis(500));
-        try!(output.set_value(1));
-        // Send 10us pulse to trigger
-        sleep(Duration::from_millis(10));
-        start = Instant::now();
-        output.set_value(0)
-    }).expect("Export gpio 23");
+    let mut elapsed_ms : f64 = -1.0;
 
     input.with_exported(|| {
         try!(input.set_direction(Direction::In));
         try!(input.set_edge(Edge::BothEdges));
         let mut poller = try!(input.get_poller());
+        output.with_exported(|| {
+            try!(output.set_direction(Direction::Out));
+            try!(output.set_value(0));
+            // Allow module to settle
+            sleep(Duration::from_millis(1000));
+            try!(output.set_value(1));
+            // Send 10us pulse to trigger
+            sleep(Duration::from_millis(20));
+            output.set_value(0);
+            start = Instant::now();
+            Ok(())
+        }).expect("Export gpio 23");
+
         loop {
             match try!(poller.poll(1000)) {
                 Some(value) => {
@@ -36,7 +37,12 @@ fn main() {
                         "poll value: {}",
                         value
                     );
-                    elapsed_ms = start.elapsed().subsec_nanos() as f64;
+                    if value == 0 {
+                        elapsed_ms = start.elapsed().subsec_nanos() as f64;
+                        break;
+                    } else {
+                        start = Instant::now();
+                    }
                 },
                 None => {break;}
             };
