710c5c52fe99b84ade65ae50869a2e84596bf7d5
Pierre Verkest <pverkest@anybox.fr>
2016-10-22 21:56:50 +0200

    [rust]some fixing

--- rust/ultrasonic/src/src/main.rs
+++ rust/ultrasonic/src/src/main.rs
@@ -9,26 +9,28 @@ use cupi::{CuPi, Logic};
 fn main() {
     let cupi = CuPi::new().unwrap();
 
-    let output = cupi.pin(23).unwrap().output();
-    let input = cupi.pin(24).unwrap().input();
+    let output = cupi.pin(4).unwrap().output();
+    let input = cupi.pin(5).unwrap().input();
     output.write(Logic::Low);
 
     // Allow module to settle
     sleep(Duration::from_millis(500));
 
 
-    let elapsed_ms : f64;
+    let elapsed: f64;
 
     output.write(Logic::High);
     // Send 10us pulse to trigger
     sleep(Duration::new(0, 10));
-    let start = Instant::now();
     output.write(Logic::Low);
+    let mut start = Instant::now();
 
     // println!("wainting High");
     loop {
         match input.read() {
-            Logic::Low => {},
+            Logic::Low => {
+                start = Instant::now();
+            },
             Logic::High => {
                 break;
             }
@@ -39,14 +41,14 @@ fn main() {
         match input.read() {
             Logic::High=> {},
             Logic::Low => {
-                elapsed_ms = start.elapsed().subsec_nanos() as f64;
+                elapsed = start.elapsed().subsec_nanos() as f64;
                 break;
             }
         };
     }
     // Distance = elapsed time / 2 * velocity
     // where velocity = 340m / s
-    let distance = elapsed_ms * 0.000000001 / 2.0 * 340.0;
+    let distance = elapsed * 0.000000001 / 2.0 * 340.0;
 
     println!("Distance {} (en m)", distance);
 }
